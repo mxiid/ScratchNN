043f8dad46738eff4eff5efcf5eb0817e6e06726
Muid bin Aslam <86907057+mxiid@users.noreply.github.com>
2024-09-08 21:40:10 +0500

    Renamed

--- nn.py
+++
@@ -1,9 +0,0 @@
-"""
-Setting up a simple neuron
-"""
-
-inputs = [1.2, 5.1, 2.1] #input from the previous neuron
-weights = [3.1, 2.1, 8.7] #every input has a weight associated with it
-bias = 3 #every unique neuron has a unique bias
-
-output = inputs[0]*weights[0]+inputs[1]*weights[1]+inputs[2]*weights[2] + bias 
